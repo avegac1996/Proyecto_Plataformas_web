ec14c245ac8c1cddf612025973fb7a7e59b01ee5
avegac1996 <anthony_vega95@hotmail.com>
2021-12-28 13:15:48 -0500

    angular

---
+++ src/app/components/cabezera/cabezera.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabezera',
+  templateUrl: './cabezera.component.html',
+  styles: [
+  ]
+})
+export class CabezeraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
