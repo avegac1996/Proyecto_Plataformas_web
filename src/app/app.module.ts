d50e228ca15aab03687b379ee0a7f6430f9426e0
avegac1996 <anthony_vega95@hotmail.com>
2022-01-01 14:42:39 -0500

    stilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +11,22 @@ import { HomeComponent } from './components/home/home.component';
 import { CabezeraComponent } from './components/cabezera/cabezera.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CabezeraComponent,
     FooterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
